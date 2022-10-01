68e8d69ac4d72729a92ef573788ddf0b91d60e4d
0xLucca <0xlucca.dev@gmail.com>
2022-09-30 22:28:36 -0300

    Contract Interaction on Message

--- clase1/webPage/clase1_dapp/components/String.js
+++ clase1/webPage/clase1_dapp/components/String.js
@@ -1,17 +1,63 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
+import { reviewsABI } from "../utils/abis/reviewsABI.js";
+import { reviewContractAddress } from "../utils/addresses";
+import { ethers } from "ethers";
+import {
+  useContractEvent,
+  useContractRead,
+  useContractWrite,
+  usePrepareContractWrite,
+} from "wagmi";
 
 const String = () => {
-  const [string, setstring] = useState('');
+  const [string, setString] = useState("");
+  const [stringInput, setStringInput] = useState("");
+
+  const contractRead = useContractRead({
+    addressOrName: reviewContractAddress,
+    contractInterface: reviewsABI,
+    functionName: "readMessage",
+    cacheOnBlock: true,
+    onSuccess(data) {
+      setString(data);
+      console.log("contractRead");
+    },
+  });
+
+  useContractEvent({
+    addressOrName: reviewContractAddress,
+    contractInterface: reviewsABI,
+    eventName: "MessageUpdated",
+    listener: (event) => {
+      console.log(event);
+      setString(event[0]);
+      console.log(string);
+    },
+  });
+
+  const { config } = usePrepareContractWrite({
+    addressOrName: reviewContractAddress,
+    contractInterface: reviewsABI,
+    functionName: "writeMessage",
+    args: [stringInput],
+  });
+  const { data, isLoading, isSuccess, write } = useContractWrite({
+    ...config,
+    onSettled(data, error) {
+      console.log("onSettled");
+    },
+  });
+
   return (
     <div>
       <p className="text-finanflixWhite font-extrabold my-[30px] text-[42px]">
-        Ingrese un texto
+        Ingrese un mensaje
       </p>
       <form className="w-10/12 m-auto">
         <input
           type="text"
-          onChange={(e) => setstring(e.target.value)}
-          placeholder="Ingrese aqui"
+          onChange={(e) => setStringInput(e.target.value)}
+          placeholder="Ingrese aqui su mensaje"
           className="w-full h-[70px] border-[4px] bg-finanflixBlack border-finanflixOrange text-finanflixWhite text-[20px] px-5 font-bold flex"
         />
 
