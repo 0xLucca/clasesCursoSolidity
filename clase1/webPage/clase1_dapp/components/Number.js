5dcf2e7916f08b0fc1e785443f197c78939a1d9c
Gera Barboni <gerardobarboni@gmail.com>
2022-10-03 15:51:41 -0300

    reset form v1

--- clase1/webPage/clase1_dapp/components/Number.js
+++ clase1/webPage/clase1_dapp/components/Number.js
@@ -1,38 +1,75 @@
 import { Input, Info } from '@web3uikit/core';
-import { useState } from 'react';
-import styles from '../styles/Home.module.css';
+import { useRef, useState } from 'react';
+import { reviewsABI } from '../utils/abis/reviewsABI.js';
+import { reviewContractAddress } from '../utils/addresses';
+import { ethers } from 'ethers';
+import {
+  useContractEvent,
+  useContractRead,
+  useContractWrite,
+  usePrepareContractWrite,
+} from 'wagmi';
 
 const Number = () => {
-  const [number, setnumber] = useState(0);
-  const [showNumber, setshowNumber] = useState(false);
+  const [number, setNumber] = useState(null);
+  const [numberInput, setNumberInput] = useState(null);
+  const clearInput = useRef();
 
-  const handleInput = () => {
-    setshowNumber(!showNumber);
-  };
+  const contractRead = useContractRead({
+    addressOrName: reviewContractAddress,
+    contractInterface: reviewsABI,
+    functionName: 'readNumber',
+    cacheOnBlock: true,
+    onSuccess(data) {
+      setNumber(ethers.utils.formatUnits(data, 0));
+      console.log('contractRead');
+    },
+  });
+
+  useContractEvent({
+    addressOrName: reviewContractAddress,
+    contractInterface: reviewsABI,
+    eventName: 'NumberUpdated',
+    listener: (event) => {
+      setNumber(ethers.utils.formatUnits(event[0], 0));
+    },
+  });
+
+  const { config } = usePrepareContractWrite({
+    addressOrName: reviewContractAddress,
+    contractInterface: reviewsABI,
+    functionName: 'writeNumber',
+    args: [numberInput],
+  });
+  const { data, isLoading, isSuccess, write } = useContractWrite({
+    ...config,
+    onSettled(data, error) {
+      console.log('onSettled');
+      clearInput.current.value = null;
+    },
+  });
+
   return (
-    <div className="">
+    <div>
       <p className="uppercase text-finanflixWhite font-extrabold my-[30px] text-[42px]">
         Ingrese un numero
       </p>
       <form className="w-10/12 m-auto">
-        <div className="flex flex-col gap-5 md:flex-row justify-between">
-          <div className="w-full h-[120px] border-[4px] border-finanflixWhite text-finanflixWhite text-[58px] font-bold flex">
-            {showNumber ? (
-              <p className="m-auto">{number}</p>
-            ) : (
-              <p className="m-auto">0</p>
-            )}
+        <div className="flex justify-between gap-5">
+          <div className="w-full h-[120px] border-[3px] border-finanflixWhite text-finanflixWhite text-[58px] font-bold flex">
+            <p className="m-auto">{number}</p>
           </div>
           <input
             type="number"
-            onChange={(e) => setnumber(e.target.value)}
-            className="w-full h-[120px] border-[4px] bg-finanflixPurple border-finanflixOrange text-finanflixWhite text-[58px] text-center font-bold flex"
+            ref={clearInput}
+            onChange={(e) => setNumberInput(e.target.value)}
+            className="w-full h-[120px] border-[3px] bg-finanflixPurple border-finanflixOrange text-finanflixWhite text-[58px] text-center font-bold flex"
           />
         </div>
         <button
           type="button"
-          className="bg-finanflixOrange w-full py-4 font-bold text-[18px] text-finanflixBlack hover:bg-finanflixBlack hover:text-finanflixOrange hover:shadow transition duration-150 mt-10"
-          onClick={() => handleInput()}
+          className="bg-finanflixOrange w-full py-4 font-bold text-[18px] text-finanflixBlack mt-10"
+          onClick={() => write()}
         >
           INGRESAR
         </button>
