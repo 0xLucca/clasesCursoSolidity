f268d195b11e4e2ffd059f1885ca8ea6576a34a3
0xLucca <0xlucca.dev@gmail.com>
2022-10-16 00:40:35 -0300

    Purchase function kinda works

--- clase2/webPage/clase2_dapp/components/Client.js
+++ clase2/webPage/clase2_dapp/components/Client.js
@@ -1,8 +1,40 @@
 import Image from "next/image";
-import React from "react";
+import React, { useEffect, useState, useRef } from "react";
 import bottle from "../assets/imgs/bottle_1.png";
+import {
+  useContractRead,
+  useContractEvent,
+  usePrepareContractWrite,
+  useContractWrite,
+} from "wagmi";
+import { ethers } from "ethers";
+import { vendingMachineABI } from "../utils/abis/vendingMachineABI.js";
+import { vendingMachineContractAddress } from "../utils/addresses";
 
-const Client = () => {
+const Client = ({ unitPrice }) => {
+  const clearInput1 = useRef();
+  const clearInput2 = useRef();
+  const [amountInput, setAmountInput] = useState(1);
+
+  const { config } = usePrepareContractWrite({
+    addressOrName: vendingMachineContractAddress,
+    contractInterface: vendingMachineABI,
+    functionName: "purchase",
+    args: [amountInput],
+    overrides: {
+      //value: ethers.utils.parseEther(unitPrice).mul(amountInput),
+      value: ethers.utils.parseEther("0.01").mul(1),
+    },
+  });
+
+  const { data, isLoading, isSuccess, write } = useContractWrite({
+    ...config,
+    onSettled(data, error) {
+      console.log("onSettled");
+      clearInput1.current.value = null;
+      setAmountInput(null);
+    },
+  });
   return (
     <div className="w-full">
       <p className="uppercase text-center font-extrabold my-[30px] text-[42px] text-finanflixWhite">
@@ -14,12 +46,22 @@ const Client = () => {
       </div>
       <input
         type="number"
-        onChange={() => {}}
+        ref={clearInput1}
+        onChange={(e) => {
+          setAmountInput(e.target.value);
+        }}
         placeholder="1"
         min={1}
         className="w-full border-[3px] bg-finanflixPurple border-finanflixOrange text-finanflixWhite text-[36px] text-center font-semibold flex py-5"
       />
-      <button className="bg-finanflixOrange w-full py-4 font-bold text-[18px] text-finanflixBlack hover:bg-finanflixBlack hover:text-finanflixOrange hover:shadow transition duration-150 mt-5">
+      <button
+        type="button"
+        className="bg-finanflixOrange w-full py-4 font-bold text-[18px] text-finanflixBlack hover:bg-finanflixBlack hover:text-finanflixOrange hover:shadow transition duration-150 mt-5"
+        onClick={() => {
+          console.log("a");
+          write?.();
+        }}
+      >
         comprar
       </button>
       <div className="mt-5">
