b6401936e66c9b0825c46308afe5d65be69780b8
0xLucca <0xlucca.dev@gmail.com>
2022-11-14 23:50:00 -0300

    Add api key for testing

--- clase2/webPage/clase2_dapp/pages/_app.js
+++ clase2/webPage/clase2_dapp/pages/_app.js
@@ -8,15 +8,13 @@ import {
 } from "@rainbow-me/rainbowkit";
 import { chain, configureChains, createClient, WagmiConfig } from "wagmi";
 import { alchemyProvider } from "wagmi/providers/alchemy";
-import { publicProvider } from "wagmi/providers/public";
 
 const { chains, provider, webSocketProvider } = configureChains(
   [chain.goerli],
   [
     alchemyProvider({
-      alchemyId: "_gg7wSSi0KMBsdKnGVfHDueq6xMB9EkC",
+      apiKey: "lPIlk3RwIrWNc6_VkykqiBw4rcrkcZ4G",
     }),
-    publicProvider(),
   ]
 );
 
