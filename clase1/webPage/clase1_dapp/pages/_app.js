c951761d0f243182b58377e6521b232668c77d29
0xLucca <0xlucca.dev@gmail.com>
2022-12-04 16:50:46 -0300

    Replace testing api key

--- clase1/webPage/clase1_dapp/pages/_app.js
+++ clase1/webPage/clase1_dapp/pages/_app.js
@@ -1,27 +1,26 @@
-import '../styles/globals.css';
-import '@rainbow-me/rainbowkit/styles.css';
+import "../styles/globals.css";
+import "@rainbow-me/rainbowkit/styles.css";
 import {
   RainbowKitProvider,
   getDefaultWallets,
   darkTheme,
   Theme,
-} from '@rainbow-me/rainbowkit';
-import { chain, configureChains, createClient, WagmiConfig } from 'wagmi';
-import { alchemyProvider } from 'wagmi/providers/alchemy';
-import { publicProvider } from 'wagmi/providers/public';
+} from "@rainbow-me/rainbowkit";
+import { chain, configureChains, createClient, WagmiConfig } from "wagmi";
+import { alchemyProvider } from "wagmi/providers/alchemy";
+import { publicProvider } from "wagmi/providers/public";
 
 const { chains, provider, webSocketProvider } = configureChains(
   [chain.goerli],
   [
     alchemyProvider({
-      alchemyId: '_gg7wSSi0KMBsdKnGVfHDueq6xMB9EkC',
+      apiKey: "lPIlk3RwIrWNc6_VkykqiBw4rcrkcZ4G",
     }),
-    publicProvider(),
   ]
 );
 
 const { connectors } = getDefaultWallets({
-  appName: 'Clase1_Dapp',
+  appName: "Clase1_Dapp",
   chains,
 });
 
@@ -39,11 +38,11 @@ function MyApp({ Component, pageProps }) {
         chains={chains}
         modalSize="compact"
         theme={darkTheme({
-          accentColor: '#FF3F00',
-          accentColorForeground: '#DEDCE5',
-          borderRadius: 'none',
-          fontStack: 'system',
-          overlayBlur: 'small',
+          accentColor: "#FF3F00",
+          accentColorForeground: "#DEDCE5",
+          borderRadius: "none",
+          fontStack: "system",
+          overlayBlur: "small",
         })}
       >
         <Component {...pageProps} />
